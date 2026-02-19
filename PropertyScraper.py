40189fb570f458900d837251f35e258064523fb1
aryansethi21052001 <imaryan@ust.hk>
2026-02-19 22:48:35 +0800

    Update PropertyScraper.py

--- PropertyScraper.py
+++ PropertyScraper.py
@@ -18,6 +18,10 @@ class PropertyScraper:
             self.service = Service(ChromeDriverManager().install())
             chrome_options = Options()
             chrome_options.add_argument("--headless=new")
+            chrome_options.add_argument("--no-sandbox")
+            chrome_options.add_argument("--disable-dev-shm-usage")
+            chrome_options.add_argument("--disable-gpu")
+            chrome_options.add_argument("--disable-extensions")
             self.driver = webdriver.Chrome(service=self.service, options=chrome_options)
             self.wait = WebDriverWait(self.driver, 10)
             self.url = "https://www.squarefoot.com.hk/en/rent" 
